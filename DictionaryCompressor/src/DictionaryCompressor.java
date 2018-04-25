2ba660a4e8635dfe5f5dbf8da88d6816fca12eae
Dogan Ervin <doganervin@gmail.com>
2018-04-25 17:33:11 -0500

    Making it loop on itself

--- DictionaryCompressor/src/DictionaryCompressor.java
+++ DictionaryCompressor/src/DictionaryCompressor.java
@@ -15,20 +15,31 @@ public class DictionaryCompressor
 	
 	static String[] baseInfo = {"0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P"};
 	
-	static String subString;
-	static String subStringRemoved;
+	static String subString, subStringRemoved, input, inputSaver, bigStringSaver, smallStringSaver;
 	
 	static int length = 0;
-    static int base = 18;
+    static int base = 36;
     static int saved = 0;
+    
+    static boolean done = false;
+    
 	public static void main(String[] args)
 	{
-        String str = "ababcdefg";
-        longSubString(str, str.length());
+        input = "jfp4qp893gegh9pw85hp89turuhkgsldjmvh59vhori4398j493tup984t5uhgopuyt9pq384gh9erhgp9q85utqp598tuqp598";
+        inputSaver = input;
+		do
+		{
+        longSubString(input, input.length());
+		replace();
+        System.out.println(input);
+        newSubstrings.clear();
+		}while(!done);
+		System.out.println("You Saved: " + Integer.toString(inputSaver.length()-input.length()) + " Characters");
+		
 	}
     private static void longSubString(String str, int n)
     {
-    	String stringSaver = "";
+    	bigStringSaver = "";
     	for (int i = 0; i < n; i++) 
     		for (int j = i+1; j <= n; j++)
     		{
@@ -38,35 +49,49 @@ public class DictionaryCompressor
         	   if(str.substring(0, i).contains(subString)||str.substring(j, str.length()).contains(subString))
         	   {
         		   repeatedSubstrings.add(subString);
-        		   if(subString.length() > stringSaver.length())
-        		   		stringSaver = subString;
+        		   if(subString.length() > bigStringSaver.length())
+        		   		bigStringSaver = subString;
         	   }
     		   uniqueSubstrings.add(subString);
     		}
     	//tempNewSubstrings.add("");
-    	shortSubString(stringSaver, n, stringSaver.length());
-    	System.out.println(uniqueSubstrings);
-    	System.out.println(repeatedSubstrings);
-    	System.out.println(stringSaver);
-
+    	shortSubString(bigStringSaver, n, bigStringSaver.length());
+    	System.out.println("unique: " + uniqueSubstrings);
+    	System.out.println("repeated: "+repeatedSubstrings); //all the strings that repeat
+    	System.out.println(bigStringSaver); //longest repeated string
     }
-    private static void shortSubString(String stringSaver, int n, int o)
+    private static void shortSubString(String bigStringSaver, int n, int o)
     {
-    	System.out.println(Integer.toString(Integer.parseInt("303245", 10), base));
+    	//System.out.println(Integer.toString(Integer.parseInt("303245", 10), base));
+		String add;
     	for (int i = 0; true; i++)
     	{
-    		tempNewSubstrings.add(Integer.toString(Integer.parseInt(Integer.toString(i), 10), base));
-    		if(tempNewSubstrings.get(i).length() >= 3)
+    		add = Integer.toString(Integer.parseInt(Integer.toString(i), 10), base);
+    		if(add.length() < 3)
+    			tempNewSubstrings.add(add);
+    		else
     			break;
     	}
-    	System.out.println(tempNewSubstrings);
+    	System.out.println("tempnew 1 : "+tempNewSubstrings);
     	for( int j = 0; j < uniqueSubstrings.size(); j++)
-    	for (int i = 0; i < tempNewSubstrings.size(); i++)
-    		if(uniqueSubstrings.get(j).contains(tempNewSubstrings.get(i)))
-    		{
-    			tempNewSubstrings.remove(i);
-    			i = i-1;
-    		}
-		System.out.println(tempNewSubstrings);
+    		for (int i = 0; i < tempNewSubstrings.size(); i++)
+    			if(uniqueSubstrings.get(j).contains(tempNewSubstrings.get(i)))
+    			{
+    				tempNewSubstrings.remove(i);
+    				i = i-1;
+    			}
+		System.out.println("tempnew 2 : "+tempNewSubstrings);
+			smallStringSaver = tempNewSubstrings.get(0);
+    }
+    private static void replace()
+    {
+    	System.out.println(bigStringSaver + smallStringSaver);
+    	if(bigStringSaver.length() > smallStringSaver.length())
+    		input = input.replaceAll(bigStringSaver, smallStringSaver); //Im worried that the replaceAll method may not be the best to use. Needs to be iterative
+    	else
+    	{
+    		System.out.println("No Efficient Solution");
+    		done = true;
+    	}
     }
 }
